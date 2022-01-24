edc654b3aac5bc0b581118f8b047549ef86404d5
tionex22 <tionex22@mail.ru>
2022-01-24 20:25:39 +0300

    Задание 2

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,9 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+  public static void main(String[] args) {
+
+    PointRun D = new PointRun(-2.3, 4, 8.5, 0.7);
+    System.out.println("Расстояние между между 2 точками на плоскости = " + D.distance());
+  }
+}
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PointRun.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.sandbox;
+
+public class PointRun {
+  public double x1;
+  public double y1;
+  public double x2;
+  public double y2;
+
+  public PointRun(double p1, double p2, double p3, double p4) {
+    this.x1 = p1;
+    this.y1 = p2;
+    this.x2 = p3;
+    this.y2 = p4;
+
+  }
+
+  public double distance() {
+    return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+  }
+}
