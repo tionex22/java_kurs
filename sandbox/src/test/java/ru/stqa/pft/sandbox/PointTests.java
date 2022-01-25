fbc0c706d2a50dd80799a2aab8f6a2fb372760b5
tionex22 <tionex22@mail.ru>
2022-01-25 18:58:20 +0300

    Задание 3

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -1,4 +1,28 @@
 package ru.stqa.pft.sandbox;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.logging.XMLFormatter;
+
 public class PointTests {
+
+  @Test
+  public void testPoint() {
+    PointRun D = new PointRun(-2.3, 4, 8.5, 0.7);
+    Assert.assertEquals(D.distance(), 11.292918134831227);
+  }
+
+  @Test
+  public void testPoint2() {
+    PointRun D = new PointRun(-2.3, 4, 8.5, 0.7);
+    Assert.assertNotNull (D.distance());
+  }
+
+  @Test
+  public void testPoint3() {
+    PointRun D = new PointRun(-2.3, 4, 8.5, 0.7);
+    double X = 11.292918134831227;
+    assert D.distance() == X;
+  }
 }
