431eee6b5f78c58d9d1e88d9cacb41b7519697f9
tionex22 <tionex22@mail.ru>
2022-03-01 19:40:07 +0300

    Лекция 6.3

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -1,7 +1,9 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model.ContactData;
-import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -11,32 +13,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ContactDataGenerator {
-  public static void main(String[] args) throws IOException {
-    int count = Integer.parseInt(args[0]);
-    File file = new File(args[1]);
 
-    List<ContactData> contacts = generateContacts(count);
-    save(contacts, file);
+  @Parameter(names = "-c", description = "Contact count")
+  public int count;
+
+  @Parameter(names = "-f", description = "Target file")
+  public String file;
+
+  public static void main(String[] args) throws IOException {
+    ContactDataGenerator generator = new ContactDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch (ParameterException ex) {
+      jCommander.usage();
+      return;
+    }
+    generator.run();
   }
 
-  private static void save (List<ContactData> contacts, File file) throws IOException {
+  private void run() throws IOException {
+    List<ContactData> contacts = generateContacts(count);
+    save(contacts, new File(file));
+  }
+
+  private void save (List<ContactData> contacts, File file) throws IOException { //Записываем данные
     System.out.println(new File(".").getAbsoluteFile());
     Writer writer = new FileWriter(file);
     for (ContactData contact : contacts) {
-      writer.write(String.format("%s;%s,%s;%s;%s,%s;%s;%s,%s\n", contact.getName(), contact.getLastname(), contact.getAddress()
+      writer.write(String.format("%s;%s,%s;%s;%s,%s;%s;%s,%s;%s\n", contact.getName(), contact.getLastname(), contact.getAddress()
               , contact.getEmail(), contact.getEmail2(), contact.getEmail3()
-              , contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone()));
+              , contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone()
+              , contact.getPhoto()));
     }
     writer.close();
   }
 
-  private static List<ContactData> generateContacts(int count) {
+  private List<ContactData> generateContacts(int count) { //Генерируем данные
     List<ContactData> contacts = new ArrayList<ContactData>();
     for (int i = 0; i < count; i++) {
       contacts.add(new ContactData().withName(String.format("name %s", i))
               .withLastName(String.format("lastname %s", i)).withAddress(String.format("address %s", i))
               .withEmail(String.format("email %s", i)).withEmail2(String.format("email2 %s", i)).withEmail3(String.format("email3 %s", i))
-              .withHomePhone(String.format("home_phone %s", i)).withMobilePhone(String.format("mobilephone %s", i)).withWorkPhone(String.format("workphone %s", i)));
+              .withHomePhone(String.format("homephone %s", i)).withMobilePhone(String.format("mobilephone %s", i)).withWorkPhone(String.format("workphone %s", i))
+              .withPhoto(new File(String.format("photo %s", i))));
     }
     return contacts;
   }
