3a3986369c4e4b5fdfc0ad6a9583f14d4cd744fc
tionex22 <tionex22@mail.ru>
2022-03-15 16:09:36 +0300

    8.3 Комментарии

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
@@ -22,21 +22,21 @@ public class HttpSession {
 
   public HttpSession(ApplicationManager app) {
     this.app = app;
-    httpclient = HttpClients.custom().setRedirectStrategy(new LaxRedirectStrategy()).build();
+    httpclient = HttpClients.custom().setRedirectStrategy(new LaxRedirectStrategy()).build(); //Шаблон для работы по http request с перенаправлениями "redirect"
   }
-  public boolean login(String username, String password) throws IOException {
-    HttpPost post = new HttpPost(app.getProperty("web.baseUrl") + "/login.php");
-    List<BasicNameValuePair> params = new ArrayList<>();
+  public boolean login(String username, String password) throws IOException { //Метод post для login
+    HttpPost post = new HttpPost(app.getProperty("web.baseUrl") + "/login.php"); //Метод post на url /login, с парметрами ниже
+    List<BasicNameValuePair> params = new ArrayList<>(); //Переводим параметры в список для отправки
     params.add(new BasicNameValuePair("username", username));
     params.add(new BasicNameValuePair("password", password));
     params.add(new BasicNameValuePair("secure_session", "on"));
     params.add(new BasicNameValuePair("return", "index.php"));
-    post.setEntity(new UrlEncodedFormEntity(params));
-    CloseableHttpResponse response = httpclient.execute(post);
-    String body = getTextFrom(response);
-    return body.contains(format("<span class=\"user-info\">%s</span>", username));
+    post.setEntity(new UrlEncodedFormEntity(params)); // Параметры "выше" упаковываются в params.setEntity
+    CloseableHttpResponse response = httpclient.execute(post); //Отправка запроса "HttpPost post" и получаем ответ в "response"
+    String body = getTextFrom(response); //Полчучаем текст из response
+    return body.contains(format("<span class=\"user-info\">%s</span>", username)); //Проверяем что в тексте содержится нужный фрагмент "username"
   }
-  private String getTextFrom(CloseableHttpResponse response) throws IOException {
+  private String getTextFrom(CloseableHttpResponse response) throws IOException { //Функция получения текста из response
     try {
       return EntityUtils.toString(response.getEntity());
     } finally {
@@ -44,9 +44,9 @@ public class HttpSession {
     }
   }
   public boolean isLoggedInAs(String username) throws IOException {
-    HttpGet get = new HttpGet(app.getProperty("web.baseUrl") + "/index.php");
-    CloseableHttpResponse response = httpclient.execute(get);
-    String body = getTextFrom(response);
-    return body.contains(format("<span class=\"user-info\">%s</span>", username));
+    HttpGet get = new HttpGet(app.getProperty("web.baseUrl") + "/index.php"); //Метод get на /index.php
+    CloseableHttpResponse response = httpclient.execute(get); //Выполняем запрос и получаем ответ в "response"
+    String body = getTextFrom(response); //Полчучаем текст из response
+    return body.contains(format("<span class=\"user-info\">%s</span>", username)); //Проверяем что в тексте содержится нужный фрагмент "username"
   }
 }
