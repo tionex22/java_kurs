e64ee31b801e7b7eebed514496fb30a309804bec
tionex22 <tionex22@mail.ru>
2022-03-15 19:55:16 +0300

    Лекция 8.6

--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -5,5 +5,7 @@ $g_database_name = 'bugtracker';
 $g_db_username = 'root';
 $g_db_password = '';
 $g_signup_use_captcha = OFF;
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'localhost';
 
 $g_crypto_master_salt = 'z0CoBRGPKu3X6z0kIUkcrK/AFXYiHsi3tKZJrw912J8=';
