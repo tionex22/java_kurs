aca893a4e973b14c7d772a1c52f827550dbeb97f
tionex22 <tionex22@mail.ru>
2022-03-23 14:51:52 +0300

    19 исправление "skipIfNotFixed"

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
@@ -14,6 +14,7 @@ public class SoapTests extends TestBase{
 
     @Test
     public void testGetProjects() throws MalformedURLException, ServiceException, RemoteException {
+        skipIfNotFixed(1);
         Set<Project> projects = app.soap().getProjects();
         System.out.println(projects.size());
         for (Project project: projects) {
