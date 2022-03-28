c5ad7f16a49f6acfea2b2a2e6dccdae85ee7f706
tionex22 <tionex22@mail.ru>
2022-03-28 17:04:41 +0300

    10.2

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -6,11 +6,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -37,6 +40,18 @@ public class ApplicationManager {
 
     dbHelper = new DbHelper();
 
+    if ("".equals(properties.getProperty("selenium.server"))) {
+      if (browser.equals(BrowserType.CHROME)) {
+        wd = new ChromeDriver();
+      } else if (browser.equals(BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.EDGE)) {
+        wd = new EdgeDriver();
+      } else {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName(browser);
+        wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);}
+    }
     if (browser.equals(BrowserType.CHROME)) {
       System.setProperty("webdriver.chrome.driver", "D:/chromedriver.exe");
       wd = new ChromeDriver();
