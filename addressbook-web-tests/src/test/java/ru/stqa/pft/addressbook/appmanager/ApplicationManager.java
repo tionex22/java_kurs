8d83f5070d0d1c2deee2326ff1bf4d6ade93c96c
tionex22 <tionex22@mail.ru>
2022-03-28 17:12:24 +0300

    10.3

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -50,6 +51,7 @@ public class ApplicationManager {
       } else {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setBrowserName(browser);
+        capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win10")));
         wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);}
     }
     if (browser.equals(BrowserType.CHROME)) {
